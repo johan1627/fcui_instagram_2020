3b595e4090c8e065d5e05a8f7eb970ce89a60ef4
johan1627 <johanns.sandida@gmail.com>
2020-09-09 22:42:02 +0700

    progress homepage

---
+++ lib/misc/constant.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// Text Colors
+// Color primaryColor = Color(0xFF1976D2);
+Color greyDark = Color(0xFF212121);
+// Color greyLight = Color(0xFFECEFF1);
+
+// Color greenColor = Color(0xFF2E7D32);
+
+// Color blueDark = Color(0xFF1976D2);
+// Color blueLight = Color(0xFFE1F5FE);
+
+Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Colors.black;
+
+// Text Styles
+TextStyle h1 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 20.0,
+);
+
+TextStyle h2 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 18.0,
+);
+
+TextStyle h3 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 16.0,
+);
+
+TextStyle h4 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 14.0,
+);
+
+TextStyle lableFont = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 12.0,
+);
+
+TextStyle footFont = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 11.0,
+);
+
+// Logo
+String logoInstagram = "assets/icons/icon_instagram.svg";
+String logoInstagramtext = "assets/icons/icon_instagramtext.svg";
+
+// Icon
+String iconTelegram = "assets/icons/icon_telegram.svg";
+
+String iconHeart = "assets/icons/icon_heart.svg";
+String iconComment = "assets/icons/icon_comment.svg";
+String iconBookmark = "assets/icons/icon_bookmark.svg";
